fcd7da252b14ba348e44ee01b6e3385c8ec6a7fb
Happy-Grass <3564494961@qq.com>
2023-03-20 12:18:26 +0800

    ascii show image

---
+++ nvim/lua/plugin-config/image.lua
@@ -0,0 +1,15 @@
+-- Important you need to install ascii-image-converter
+return function()
+	require("image").setup({
+		render = {
+			min_padding = 5,
+			show_label = true,
+			use_dither = true,
+			foreground_color = false,
+			background_color = false,
+		},
+		events = {
+			update_on_nvim_resize = true,
+		},
+	})
+end
