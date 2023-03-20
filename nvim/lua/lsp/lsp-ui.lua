aecb3437cfd5c12651912c24b6e5304e48907c1b
Happy-Grass <3564494961@qq.com>
2023-03-20 12:17:57 +0800

    lsp-ui change

--- nvim/lua/lsp/lsp-ui.lua
+++ nvim/lua/lsp/lsp-ui.lua
@@ -1,173 +1,173 @@
-local lspsaga = require('lspsaga')
-local lspkind = require('lspkind')
+local lspsaga = require("lspsaga")
+local lspkind = require("lspkind")
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  update_in_insert = false,
+	virtual_text = false,
+	signs = true,
+	update_in_insert = false,
 })
 -- set the code hints icons
 lspkind.init({
-    mode = 'symbol_text',
-    preset = 'codicons',
-    symbol_map = {
-      Text = "",
-      Method = "",
-      Function = "",
-      Constructor = "",
-      Field = "ﰠ",
-      Variable = "",
-      Class = "ﴯ",
-      Interface = "",
-      Module = "",
-      Property = "ﰠ",
-      Unit = "塞",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "פּ",
-      Event = "",
-      Operator = "",
-      TypeParameter = ""
-    },
+	mode = "symbol_text",
+	preset = "codicons",
+	symbol_map = {
+		Text = "",
+		Method = "",
+		Function = "",
+		Constructor = "",
+		Field = "ﰠ",
+		Variable = "",
+		Class = "ﴯ",
+		Interface = "",
+		Module = "",
+		Property = "ﰠ",
+		Unit = "塞",
+		Value = "",
+		Enum = "",
+		Keyword = "",
+		Snippet = "",
+		Color = "",
+		File = "",
+		Reference = "",
+		Folder = "",
+		EnumMember = "",
+		Constant = "",
+		Struct = "פּ",
+		Event = "",
+		Operator = "",
+		TypeParameter = "",
+	},
 })
 
 lspsaga.setup({
-    ui = {
-      theme = "round",
-      -- this option only works in Neovim 0.9
-      title = true,
-      -- Border type can be single, double, rounded, solid, shadow.
-      border = "double",
-      winblend = 0,
-      expand = "",
-      collapse = "",
-      preview = " ",
-      code_action = "💡",
-      diagnostic = "🐞",
-      incoming = " ",
-      outgoing = " ",
-      hover = ' ',
-      kind = {},
-    },
-    preview = {
-      lines_above = 0,
-      lines_below = 10,
-    },
-    scroll_preview = {
-      scroll_down = "<C-f>",
-      scroll_up = "<C-b>",
-    },
-    request_timeout = 2000,
-    finder = {
-      max_height = 0.5,
-      keys = {
-        jump_to = 'p',
-        edit = { 'o', '<CR>' },
-        vsplit = 's',
-        split = 'i',
-        tabe = 't',
-        quit = { 'q', '<ESC>' },
-        close_in_preview = '<ESC>'
-        },
-      },
-    code_action = {
-      num_shortcut = true,
-      show_server_name = false,
-      extend_gitsigns = true,
-      keys = {
-        quit = "q",
-        exec = "<CR>",
-      },
-    },
-    lightbulb = {
-      enable = true,
-      enable_in_insert = true,
-      sign = true,
-      sign_priority = 40,
-      virtual_text = true,
-    },
-    diagnostic = {
-      show_code_action = true,
-      show_source = true,
-      jump_num_shortcut = true,
-      max_width = 0.7,
-      custom_fix = nil,
-      custom_msg = nil,
-      text_hl_follow = false,
-      border_follow = true,
-      keys = {
-        exec_action = "o",
-        quit = "q",
-        go_action = "g"
-      },
-    },
-    rename = {
-      quit = "<C-c>",
-      exec = "<CR>",
-      mark = "x",
-      confirm = "<CR>",
-      in_select = true,
-    },
-    outline = {
-      win_position = "right",
-      win_with = "",
-      win_width = 30,
-      show_detail = true,
-      auto_preview = true,
-      auto_refresh = true,
-      auto_close = true,
-      custom_sort = nil,
-      keys = {
-        jump = "o",
-        expand_collapse = "u",
-        quit = "q",
-      },
-    },
-    callhierarchy = {
-      show_detail = false,
-      keys = {
-        edit = "e",
-        vsplit = "s",
-        split = "i",
-        tabe = "t",
-        jump = "o",
-        quit = "q",
-        expand_collapse = "u",
-      },
-    },
-    symbol_in_winbar = {
-      enable = true,
-      separator = " ",
-      hide_keyword = true,
-      show_file = true,
-      folder_level = 2,
-      respect_root = false,
-      color_mode = true,
-    },
+	ui = {
+		theme = "round",
+		-- this option only works in Neovim 0.9
+		title = true,
+		-- Border type can be single, double, rounded, solid, shadow.
+		border = "double",
+		winblend = 0,
+		expand = "",
+		collapse = "",
+		preview = " ",
+		code_action = "💡",
+		diagnostic = "🐞",
+		incoming = " ",
+		outgoing = " ",
+		hover = " ",
+		kind = {},
+	},
+	preview = {
+		lines_above = 0,
+		lines_below = 10,
+	},
+	scroll_preview = {
+		scroll_down = "<C-f>",
+		scroll_up = "<C-b>",
+	},
+	request_timeout = 2000,
+	finder = {
+		max_height = 0.5,
+		keys = {
+			jump_to = "p",
+			edit = { "o", "<CR>" },
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			quit = { "q", "<ESC>" },
+			close_in_preview = "<ESC>",
+		},
+	},
+	code_action = {
+		num_shortcut = true,
+		show_server_name = false,
+		extend_gitsigns = true,
+		keys = {
+			quit = "q",
+			exec = "<CR>",
+		},
+	},
+	lightbulb = {
+		enable = true,
+		enable_in_insert = true,
+		sign = true,
+		sign_priority = 40,
+		virtual_text = true,
+	},
+	diagnostic = {
+		show_code_action = true,
+		show_source = true,
+		jump_num_shortcut = true,
+		max_width = 0.7,
+		custom_fix = nil,
+		custom_msg = nil,
+		text_hl_follow = false,
+		border_follow = true,
+		keys = {
+			exec_action = "o",
+			quit = "q",
+			go_action = "g",
+		},
+	},
+	rename = {
+		quit = "<C-c>",
+		exec = "<CR>",
+		mark = "x",
+		confirm = "<CR>",
+		in_select = true,
+	},
+	outline = {
+		win_position = "right",
+		win_with = "",
+		win_width = 30,
+		show_detail = true,
+		auto_preview = true,
+		auto_refresh = true,
+		auto_close = true,
+		custom_sort = nil,
+		keys = {
+			jump = "o",
+			expand_collapse = "u",
+			quit = "q",
+		},
+	},
+	callhierarchy = {
+		show_detail = false,
+		keys = {
+			edit = "e",
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			jump = "o",
+			quit = "q",
+			expand_collapse = "u",
+		},
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = " ",
+		hide_keyword = true,
+		show_file = true,
+		folder_level = 2,
+		respect_root = false,
+		color_mode = true,
+	},
 })
-local M ={}
+local M = {}
 -- 为 cmp.lua 提供参数格式
 M.formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      before = function (entry, vim_item)
-        -- Source 显示提示来源
-        vim_item.menu = "[" .. string.upper(entry.source.name) .. "]"
-        return vim_item
-      end
-    })
+	format = lspkind.cmp_format({
+		mode = "symbol_text",
+		maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+		before = function(entry, vim_item)
+			-- Source 显示提示来源
+			vim_item.menu = "[" .. string.upper(entry.source.name) .. "]"
+			return vim_item
+		end,
+	}),
 }
 return M
